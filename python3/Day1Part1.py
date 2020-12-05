50a35a31eb0c85c5575abf51f7d791dbc127fbb8
crnafus <cnafus1@gmail.com>
2020-12-05 12:50:11 -0700

    Setting up initial files

---
+++ python3/Day1Part1.py
@@ -0,0 +1,10 @@
+#Problem: find the two entries that sum to 2020 and then multiply those two numbers together.
+
+def main():
+    inputFile = open('Day1Part1Input.txt', 'r')
+    for line in inputFile:
+        print(line)
+
+
+if __name__ == "__main__":
+    main()

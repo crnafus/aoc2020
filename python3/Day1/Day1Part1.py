8c1c50d8b9f76ecbeb9aa501ecc1b44c132059ab
crnafus <cnafus1@gmail.com>
2020-12-05 13:18:18 -0700

    Adding in answer for day 1 part 1

--- python3/Day1/Day1Part1.py
+++ python3/Day1/Day1Part1.py
@@ -1,10 +1,18 @@
 #Problem: find the two entries that sum to 2020 and then multiply those two numbers together.
 
+import os
+
 def main():
-    inputFile = open('Day1Part1Input.txt', 'r')
-    for line in inputFile:
-        print(line)
-
-
+    inputList = []
+    with open('Day1Part1Input.txt') as openfileobject:
+        inputList = openfileobject.read().splitlines()
+    
+    for i in range(0, len(inputList)):
+        for j in range(i+1, len(inputList)):
+            if(int(inputList[i]) + int(inputList[j]) == 2020):
+                answer = int(inputList[i]) * int(inputList[j])
+                print("Numbers that add to 2020 are: " + inputList[i] + " and  " + inputList[j])
+                print("Numbers multiplied together: " + str(answer))
+        
 if __name__ == "__main__":
     main()
